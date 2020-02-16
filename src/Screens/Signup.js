0d7226de0a807fc785d57753a92334e3655cacdc
chad <canddx3@gmail.com>
2020-02-16 14:41:01 -0600

    nodes  arent working again

--- src/Screens/Signup.js
+++ src/Screens/Signup.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Charity from './Charity';
 
-
+const Signup = () => (
 function Signup () {
     var UserName = document.getElementById("userName").value;
     var Password = document.getElementById("passWord").value;
     var PostName = UserName + " " + Password;
     document.getElementById("PostName").innerHTML = PostName;
-}
+    }
+);
+
 export default Signup;
