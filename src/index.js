b5668b80c7db0e970a130d4e5559f8f728f7f134
chad <canddx3@gmail.com>
2020-02-21 15:43:33 -0600

    newer redue on layout

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Compontents/Home';
 import Charity from './Compontents/Charity';
-import Signup from './Compontents/Signup';
-import Login from './Compontents/Login';
 import Volunteer from './Compontents/Volunteer';
-import CharityList from './Compontents/CharityList';
-import Search from './Compontents/Search';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
@@ -20,11 +16,7 @@ const App = () => (
       <hr />
       <Route exact path="/" component={Home} />
       <Route exact path="/Charity" component={Charity} />
-      <Route path="/Signup" component={Signup} />
-      <Route path="/Login" component={Login} />
       <Route path="/Volunteer" component={Volunteer} />
-      <Route path="/CharityList" component={CharityList} />
-      <Route path="/Search" component={Search} />
     </div>
   </Router>
 );
