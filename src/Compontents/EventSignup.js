87046f5d59d893f1301b91e059b1db88f71c6267
Paul Stuart <pas1281@gmail.com>
2020-03-24 12:03:44 -0400

    Update option to select event time

--- src/Compontents/EventSignup.js
+++ src/Compontents/EventSignup.js
@@ -32,43 +32,93 @@ class EventSignup extends Component{
 
     render() {
         return(
-            <div>
+            <div class="container">
                 <CharityProfileBar />
                 <h2 className="text-center">Events Signup</h2>
                 <form>
                 <div className="form-group">
-                    <label>Charity Name:</label>
+                    <label classname="item-title">Charity Name:</label>
                     <input type="text" name="charityName"  value={this.state.charityName} onChange={this.onChange}/>
                 </div>
 
                 <div className="form-group">
-                    <label>Charity Phone:</label>
+                    <label classname="item-title">Charity Phone:</label>
                     <input type="text" name="charityPhone"  value={this.state.charityPhone} onChange={this.onChange}/>
                 </div>
 
                <div className="form-group">
-                    <label>Event Name:</label>
+                    <label classname="item-title">Event Name:</label>
                     <input  type="text" name="eventName"  value={this.state.eventName} onChange={this.onChange}/>
                 </div>
                 
                 <div className="form-group">
-                    <label>Event Location:</label>
+                    <label classname="item-title">Event Location:</label>
                     <input type="text" name="eventLocation"  value={this.state.eventLocation} onChange={this.onChange}/>
                 </div>
 
                 <div className="form-group">
-                    <label>Event Date:</label>
+                    <label classname="item-title">Event Date:</label>
                     <input type="text" name="eventDate"  value={this.state.eventDate} onChange={this.onChange}/>
                 </div>
 
                 <div className="form-group">
-                    <label>Event Time:</label>
-                    <input type="text"  name="eventTime"  value={this.state.eventTime} onChange={this.onChange}/>
+                    <label classname="item-title">Event Time:</label>
+                    <br></br>
+                    <select type="text"  name="eventTime"  value={this.state.eventTime} onChange={this.onChange}>
+                        <option>12:00am</option>
+                        <option>12:30am</option>
+                        <option>1:00am</option>
+                        <option>1:30am</option>
+                        <option>2:00am</option>
+                        <option>2:30am</option>
+                        <option>3:00am</option>
+                        <option>3:30am</option>
+                        <option>4:00am</option>
+                        <option>4:30am</option>
+                        <option>5:00am</option>
+                        <option>5:30am</option>
+                        <option>6:00am</option>
+                        <option>6:30am</option>
+                        <option>7:00am</option>
+                        <option>7:30am</option>
+                        <option>8:00am</option>
+                        <option>8:30am</option>
+                        <option>9:00am</option>
+                        <option>9:30am</option>
+                        <option>10:00am</option>
+                        <option>10:30am</option>
+                        <option>11:00am</option>
+                        <option>11:30am</option>
+                        <option>12:00pm</option>
+                        <option>12:30pm</option>
+                        <option>1:00pm</option>
+                        <option>1:30pm</option>
+                        <option>2:00pm</option>
+                        <option>2:30pm</option>
+                        <option>3:00pm</option>
+                        <option>3:30pm</option>
+                        <option>4:00pm</option>
+                        <option>4:30pm</option>
+                        <option>5:00pm</option>
+                        <option>5:30pm</option>
+                        <option>6:00pm</option>
+                        <option>6:30pm</option>
+                        <option>7:00pm</option>
+                        <option>7:30pm</option>
+                        <option>8:00pm</option>
+                        <option>8:30pm</option>
+                        <option>9:00pm</option>
+                        <option>9:30pm</option>
+                        <option>10:00pm</option>
+                        <option>10:30pm</option>
+                        <option>11:00pm</option>
+                        <option>11:30pm</option>
+                    </select>
                 </div>
 
                 <div className="form-group">
-                    <label>Event Description:</label>
-                    <input type="text"  name="eventDescription"  value={this.state.eventDescription} onChange={this.onChange}/>
+                    <label classname="item-title">Event Description:</label>
+                    <textarea type="text"  name="eventDescription"  value={this.state.eventDescription} onChange={this.onChange}/>
                 </div>
 
                 <button className="btn btn-success" onClick={this.saveUser}>Update</button>
